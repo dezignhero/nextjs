e0a077c4965eaf328ba22d7d71f6d668e8380bed
Alex <alex@ifxstudios.com>
2021-02-09 21:35:30 +0800

    PE-1722, MINOR - Google tracking code update

--- pages/_document.js
+++ pages/_document.js
@@ -19,11 +19,11 @@ class MyDocument extends Document {
         <Head>
           <script
             async
-            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA}`}
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_TRACKING_ID}`}
           />
           <script
             dangerouslySetInnerHTML={{
-              __html: `window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', '[Tracking ID]');`,
+              __html: `window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', '${process.env.GA_TRACKING_ID}');`,
             }}
           />
         </Head>
